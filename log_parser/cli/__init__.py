841d6639f0868113ad0c128f06d7f1f039126082
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-25 23:01:06 +0100

    Modify CLI parser

---
+++ log_parser/cli/__init__.py
@@ -0,0 +1,1 @@
+from log_parser.cli.parser import parse_args
---
+++ log_parser/cli/parser.py
@@ -0,0 +1,25 @@
+import argparse
+from typing import List
+
+
+parser = argparse.ArgumentParser(
+    prog='log-parser',
+    description='A CLI tool for parsing logs.'
+                ' Generates useful statistics and metrics based on the server log.'
+)
+
+parser.add_argument('file',
+                   type=str,
+                   help='Path to the log file.')
+
+parser.add_argument('--since',
+                   type=str,
+                   help='The date that marks the start of statistics generation.')
+
+parser.add_argument('--until',
+                   type=str,
+                   help='The date that marks the end of statistics generation.')
+
+
+def parse_args(args: List[str]) -> argparse.Namespace:
+    return parser.parse_args(args)
