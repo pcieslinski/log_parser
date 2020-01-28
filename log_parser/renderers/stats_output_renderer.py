e4c3784700954fbf8e2b30c1cb96ebfafeeff485
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-28 17:30:29 +0100

    Implement StatsOutputRenderer

---
+++ log_parser/renderers/stats_output_renderer.py
@@ -0,0 +1,27 @@
+from collections import Counter
+
+from log_parser.renderers.irenderer import IRenderer
+
+
+class StatsOutputRenderer(IRenderer):
+    def __init__(
+            self,
+            n_requests: int,
+            responses_statuses_count: Counter,
+            requests_per_second: float,
+            avg_size_for_2xx: str
+    ) -> None:
+        self.n_requests = n_requests
+        self.responses_statuses_count = responses_statuses_count
+        self.requests_per_second = requests_per_second
+        self.avg_size_for_2xx = avg_size_for_2xx
+
+    def render(self) -> None:
+        print(f'Number of requests: {self.n_requests}')
+
+        print(f'Responses statuses count:')
+        for status, count in self.responses_statuses_count.items():
+            print(f'{status}: {count}')
+
+        print(f'Requests per second: {self.requests_per_second}')
+        print(f'Average size of response for 2xx: {self.avg_size_for_2xx}')
