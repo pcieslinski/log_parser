73578d238a14b0fbea15f2d983ca5d0d6f16adf9
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-08-24 22:36:54 +0200

    Put all statistics into one module

--- log_parser/commands/stats/statistics/__init__.py
+++
@@ -1,4 +0,0 @@
-from log_parser.commands.stats.statistics.number_of_requests import NumberOfRequests
-from log_parser.commands.stats.statistics.number_of_statuses import NumberOfStatuses
-from log_parser.commands.stats.statistics.requests_per_second import RequestsPerSecond
-from log_parser.commands.stats.statistics.average_response_size import AverageResponseSize
