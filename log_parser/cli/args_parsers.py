bcfa029c73eb4d2378cf8799036fbf4fb078c26b
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-08-28 22:29:22 +0200

    Refactor args_parsers

--- log_parser/cli/args_parsers.py
+++ log_parser/cli/args_parsers.py
@@ -2,6 +2,18 @@ import argparse
 from typing import List
 
 
+main_parser = argparse.ArgumentParser(
+    prog='log',
+    description='A CLI tool for parsing logs.',
+    usage=('log-parser <command> [<args>]\n'
+           '\nlog-parser commands are:\n'
+           'stats     Generates useful statistics and metrics based on the server log.\n')
+)
+
+main_parser.add_argument('command',
+                         help='Subcommand to run')
+
+
 stats_parser = argparse.ArgumentParser(
     prog='log-parser stats',
     description='Generates useful statistics and metrics based on the server log.'
@@ -27,6 +39,10 @@ stats_parser.add_argument('--verbose',
                           '-v',
                           action='store_true',
                           help=argparse.SUPPRESS)
+
+
+def main_parse_args(args: List[str]) -> argparse.Namespace:
+    return main_parser.parse_args(args)
 
 
 def stats_parse_args(args: List[str]) -> argparse.Namespace:
